9aac205a098428e960183434a162e68f7e7e06a1
ebas17 <78296121+ebas17@users.noreply.github.com>
2025-10-01 11:59:14 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "pages.dev";
+let APP_DOMAIN = "ebasbisa";
 
 let prxIP = "";
 let cachedPrxList = [];
